f4c5686237b8e964a7edfccb4f1a79e980432e6c
Victor <barosv@yandex.ru>
2023-05-16 19:40:16 +0400

    На проверку

---
+++ src/js/script.js
@@ -0,0 +1,88 @@
+var menu = ['1', '2', '3']
+var mySwiper = new Swiper('.swiper-container', {
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.about-button-next',
+        prevEl: '.about-button-prev',
+    },
+});
+
+// burger
+const burger = document.querySelector('.header__burger');
+const menuElem = document.querySelector('.menu');
+const menuClose = document.querySelectorAll('[data-menuClose]');
+
+burger.addEventListener('click', () => {
+    menuElem.classList.add('menu--active')
+    document.body.style.overflow = 'hidden';
+})
+menuClose.forEach(item => {
+    item.addEventListener('click', () => {
+        menuElem.classList.remove('menu--active')
+        document.body.style.overflow = '';
+    })
+})
+menuElem.addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu')) {
+        menuElem.classList.remove('menu--active')
+        document.body.style.overflow = '';
+    }
+})
+
+function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scarollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scarollWidth;
+}
+
+let scrollWidth = calcScroll();
+
+function modal(modal, modalActiveClass, triggers, modalClose) {
+    const triggers_ = document.querySelectorAll(triggers),
+        modal_ = document.querySelector(modal),
+        modalClose_ = document.querySelector(modalClose);
+
+    if (triggers_.length > 0) {
+        triggers_.forEach(item => {
+            item.addEventListener('click', () => {
+                modal_.classList.add(modalActiveClass);
+                document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scrollWidth}px`;
+            });
+        });
+
+        modalClose_.addEventListener('click', () => {
+            modal_.classList.remove(modalActiveClass);
+            document.body.style.overflow = '';
+            document.body.style.marginRight = '0px';
+        });
+
+        modal_.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal__container')) {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            }
+        });
+    }
+}
+
+modal('.modal-main', 'modal--active', '[data-modal]', '.modal-main__close');
